a3d302ad8ecd950b8bf4428a914598399b11a466
eskokado <eskokado@gmail.com>
2022-06-22 14:58:39 -0300

    feat: shared preferences

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:projeto01/controllers/home_controller.dart';
 import 'package:projeto01/models/post_model.dart';
 import 'package:projeto01/repositories/home_repository_dio.dart';
+import 'package:projeto01/services/prefs_service.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
@@ -24,6 +25,18 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         centerTitle: true,
         title: const Text('Home'),
+        actions: [
+          IconButton(
+            onPressed: () {
+              PrefsService.logout();
+              Navigator.of(context).pushNamedAndRemoveUntil(
+                '/login',
+                (_) => true,
+              );
+            },
+            icon: const Icon(Icons.logout),
+          ),
+        ],
       ),
       body: ValueListenableBuilder<List<PostModel>>(
         valueListenable: _controller.posts,
