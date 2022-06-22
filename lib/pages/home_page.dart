247ba86e4e0278a33cd800b66cc57401fe17c53f
eskokado <eskokado@gmail.com>
2022-06-22 09:31:59 -0300

    feat: repository pattern

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:projeto01/controllers/home_controller.dart';
+import 'package:projeto01/models/post_model.dart';
+import 'package:projeto01/repositories/home_repository_mock.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final HomeController _controller = HomeController(HomeRepositoryMock());
+  @override
+  void initState() {
+    super.initState();
+    _controller.fetch();
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: ValueListenableBuilder<List<PostModel>>(
+        valueListenable: _controller.posts,
+        builder: (_, list, __) {
+          return ListView.builder(
+            itemCount: list.length,
+            itemBuilder: (_, index) => ListTile(
+              title: Text(list[index].title),
+            ),
+          );
+        },
+      ),
     );
   }
 }
